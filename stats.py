3ee681ad9913541132fd9d18bacdf72a7b0690d9
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2025-08-30 12:38:31 +0100

    BookBot!

--- stats.py
+++ stats.py
@@ -9,3 +9,16 @@ def char_count_dict(texts):
         char_count[text.lower()] = char_count.get(text.lower(), 0) + 1
 
     return char_count
+
+
+def sorted_list_dict(char_dict):
+    list_dict = []
+
+    def sort_on(item):
+        return item["num"]
+
+    for char, num in char_dict.items():
+        list_dict.append({"char": char, "num": num})
+
+    list_dict.sort(reverse=True, key=sort_on)
+    return list_dict
