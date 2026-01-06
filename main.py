c3ff497a737421d20fd6beceda8050ed777a645e
MrEkwunife <ekwunifechimobi16@gmail.com>
2026-01-06 07:42:54 +0100

    bookbot!

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from stats import get_char_counts, get_num_words
+import sys
+
+from stats import get_char_counts, get_num_words, get_sorted_chars_data
 
 """
 get_book_text - returns contents of a file
@@ -14,12 +16,29 @@ def get_book_text(file_path):
 
 # Program entry point
 def main():
-    file_contents = get_book_text("./books/frankenstein.txt")
-    num_words = get_num_words(file_contents)
-    chars = get_char_counts(file_contents)
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
-    print(f"Found {num_words} total words")
-    print(chars)
+    try:
+        file_contents = get_book_text(sys.argv[1])
+        num_words = get_num_words(file_contents)
+        chars = get_char_counts(file_contents)
+        sorted_char_lst = get_sorted_chars_data(chars)
+
+        print("============ BOOKBOT ============")
+        print("----------- Word Count ----------")
+        print(f"Found {num_words} total words")
+        print("--------- Character Count -------")
+
+        for char_dict in sorted_char_lst:
+            if char_dict["name"].isalpha():
+                print(f"{char_dict['name']}: {char_dict['num']}")
+
+        print("============= END ===============")
+    except FileNotFoundError:
+        print(f"{sys.argv[1]} is not in the books directory")
+        sys.exit(1)
 
 
 main()
--- stats.py
+++ stats.py
@@ -25,3 +25,30 @@ def get_char_counts(text):
         counts[c] = counts.get(c, 0) + 1
 
     return counts
+
+
+"""
+get_sorted_chars_data - takes a dictionary of chars and their counts (i.e {'a': 34, 'c': 456, ...}) and
+makes a list of dictionary out of them with each character having it own object like:
+    {"name": "a", "num": 34}, {"name": "b", "num": 456}. The list will be sorted in decending
+order.
+@char_dict:
+    dictionary of characters with their counts
+Return:
+    list of dictionary having keys "char" (for the character) and "num" to specify their count,
+    sorted in decending order based on the num
+"""
+
+
+def get_sorted_chars_data(char_dict):
+    chars_data_lst = []
+
+    def sort_on(items):
+        return items["num"]
+
+    for key, value in char_dict.items():
+        d = {"name": key, "num": value}
+        chars_data_lst.append(d)
+
+    chars_data_lst.sort(reverse=True, key=sort_on)
+    return chars_data_lst
